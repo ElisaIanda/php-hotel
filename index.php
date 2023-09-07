75e198578a766c5a7d00c57ab3728fcd26c9f46c
ElisaIanda <elisaianda01@gmail.com>
2023-09-07 17:06:06 +0200

    Stampato tutti i dati

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +56,41 @@
 
 <body>
 
+    <div class="container">
+
+        <h2 class="my-4">Risultati Hotel</h2>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance from center</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <!-- ciclo foreach per restituire valori dell'array -->
+                <?php foreach ($hotels as $singlehotel) { ?>
+
+                    <tr>
+                        <td><?php echo $singlehotel["name"] ?></td>
+
+                        <td><?php echo $singlehotel["description"] ?></td>
+                        
+                        <td><?php echo $singlehotel["parking"] ? "Yes" : "No"  ?></td>
+                        
+                        <td><?php echo $singlehotel["vote"] ?></td>
+                        
+                        <td><?php echo $singlehotel["distance_to_center"] ?> km</td>
+                    </tr>
+
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
